c6389a3576754a24b6dfb8c21595741871277022
Moonteiro <owagnerlucas0@gmail.com>
2025-12-06 09:17:09 -0300

    chore(config): adiciona caminhos para os dados gerados

---
+++ src/utils/config.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+// Caminho para o DB
+export const DATA = path.join(__dirname, '..', '..', 'data', 'news.db');
+
+// Caminho para o relatório
+export const REPORT = path.join(__dirname, '..', '..', 'data', 'report.txt');
