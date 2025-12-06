3cd85b7146284cd37961afa82e8721fb8285cefc
Moonteiro <owagnerlucas0@gmail.com>
2025-12-06 13:59:15 -0300

    refactor(interface): refatora saídas da interface

--- src/repository/iNewsRepository.ts
+++ src/repository/iNewsRepository.ts
@@ -7,27 +7,27 @@ import { News } from '../models/news'
 export interface INewsRepository {
 
   // Adiciona uma notícia ao Banco de dados
-  createNews(url: string, status: string): Promise<News>;
+  createNews(news: News): boolean;
 
   // Obtêm todas as notícias
-  getAllNews(): Promise<News[]>;
+  getAllNews(): News[];
 
   // Atualiza o status de uma notícia existente
-  updateNews(id: number, newStatus: string): Promise<boolean>;
+  updateNews(id: number, newStatus: string): boolean;
 
   // Deleta uma notícia permanentemente caso ela exista
-  deleteNews(id: number): Promise<boolean>
+  deleteNews(id: number): boolean;
 
   // Procura uma notícia pelo Id
-  getById(id: number): Promise<News | undefined>;
+  getById(id: number): News | undefined;
 
   // Busca todas as notícias com status específicos
-  getAllNewsStatus(status: string): Promise<News[]>;
+  getAllNewsStatus(status: string): News[];
 
   // Obtêm o numero total de notícias cadastradas.
-  countNews(): Promise<number>;
+  countNews(): number;
 
   // Obtêm o numero total de notícias por status.
-  countNewsStatus(status: string): Promise<number>
+  countNewsStatus(status: string): number;
 
 }
