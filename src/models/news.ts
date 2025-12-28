060d54e808852dc9bbb14b95474373c3eaefff39
Moonteiro <owagnerlucas0@gmail.com>
2025-12-28 08:16:11 -0300

    refactor(models): implementa o parâmetro opcional 'id' e modifica a interface INews para exportação

--- src/models/news.ts
+++ src/models/news.ts
@@ -1,7 +1,8 @@
 /**
  * Representa uma notícia para ser guardada.
  */
-interface INews {
+export interface INews {
+  id?: number;
   url: string;
   status: string;
 };
@@ -9,11 +10,13 @@ interface INews {
 export class News implements INews {
     /**
      * Cria uma nova notícia.
+     * @param id Id da notícia (Opcional)
      * @param url Url da notícia
      * @param status Status da notícia
     */
   constructor(
     public url: string,
-    public status: string
+    public status: string,
+    public id?: number
   ) {}
 }
